eddc6a651ae7632bbac9a8de849089eea5fafac4
Haruto <ynhrt1250@gmail.com>
2018-12-18 12:09:11 +0900

    できたっぽい

--- ble_uart.h
+++ ble_uart.h
@@ -31,9 +31,9 @@ class BleUart : public BLECharacteristicCallbacks, public BLEServerCallbacks, pu
     bool deviceConnected = false;
     bool oldDeviceConnected = false;
 
-    const char* SERVICE_UUID = "6E400001-C5A3-F393-E0A9-E50E24DCCA9E";
-    const char* CHARACTERISTIC_UUID_RX = "6E400002-C5A3-F393-E0A9-E50E24DCCA9E";
-    const char* CHARACTERISTIC_UUID_TX = "6E400003-C5A3-F393-E0A9-E50E24DCCA9E";
+    const char* SERVICE_UUID =           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+    const char* CHARACTERISTIC_UUID_RX = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+    const char* CHARACTERISTIC_UUID_TX = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
 
     uint8_t tx_counter=0;
     uint8_t buffer[100];
